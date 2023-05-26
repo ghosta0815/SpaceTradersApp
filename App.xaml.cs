2214f67d46c239fe619af33935dafb629e935cc1
Manuel Gschrey <Manuel.Gschrey@gmail.com>
2023-05-26 21:05:38 +0200

    Starting it now with DI

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,37 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Configuration;
+using System.Windows;
 
 namespace SpaceTradersApp;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
+    public static IHost? AppHost { get; private set; }
+
+    public App()
+    {
+        AppHost = Host.CreateDefaultBuilder()
+            .ConfigureServices((hostcontext, services) =>
+            {
+                services.AddSingleton<MainWindow>();
+            }).Build();
+    }
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        await AppHost!.StartAsync();
+
+        var startupForm = AppHost.Services.GetRequiredService<MainWindow>();
+        startupForm.Show();
+
+
+        base.OnStartup(e);  
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await AppHost!.StopAsync();
+        base.OnExit(e);
+    }
 }
